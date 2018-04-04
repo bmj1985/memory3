b1c27f7b2cf0ef9487a228ebe2c80297798d68b4
Brandon Johnson <bmj1985@gmail.com>
2018-04-04 11:39:04 -0600

    returned item in read() function in queries.js

--- queries.js
+++ queries.js
@@ -1,32 +1,33 @@
 const database = require('./database-connection');
 
 module.exports = {
-    returnFirstItem(arr) {
-        return arr[0];
-    },
-    list() {
-        return database('game');
-    },
-    read(id) {
-        return database('game')
-            .where('id', id);
-    },
-    create(game) {
-        return database('game')
-            .insert(game)
-            .returning('*')
-            .then(this.returnFirstItem);
-    },
-    update(id, game) {
-        return database('game')
-            .update(game)
-            .where('id', id)
-            .returning('*')
-            .then(this.returnFirstItem);
-    },
-    delete(id) {
-        return database('game')
-            .delete()
-            .where('id', id);
-    },
+  returnFirstItem(arr) {
+    return arr[0];
+  },
+  list() {
+    return database('game');
+  },
+  read(id) {
+    return database('game')
+      .where('id', id)
+      .then(this.returnFirstItem);
+  },
+  create(game) {
+    return database('game')
+      .insert(game)
+      .returning('*')
+      .then(this.returnFirstItem);
+  },
+  update(id, game) {
+    return database('game')
+      .update(game)
+      .where('id', id)
+      .returning('*')
+      .then(this.returnFirstItem);
+  },
+  delete(id) {
+    return database('game')
+      .delete()
+      .where('id', id);
+  },
 };
